6da0a079be9d40152d6d6cefe4e369fbee9b93b0
Ravi Oza <ojharavi64344@gmail.com>
2022-05-21 21:46:46 +0530

    01 copy to clipboard unity api

---
+++ Assets/01-CopyToClipboard/Scripts/CopyToClipboardComponent.cs
@@ -0,0 +1,83 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.ravio.unitylearning.copytoclipboard
+{
+    [RequireComponent(typeof(Button))]
+    public class CopyToClipboardComponent : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField]
+        private TextMeshProUGUI copyTextField;
+
+        private Button button;
+
+        #endregion
+
+        #region Mono Functions
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            AddListeners();
+        }
+
+        private void OnDisable()
+        {
+            RemoveListeners();
+        } 
+
+        #endregion
+
+        #region Listeners
+
+        private void AddListeners()
+        {
+            if (button != null)
+            {
+                button.onClick.AddListener(HandleOnButtonClicked);
+            }
+        }
+
+        private void RemoveListeners()
+        {
+            if (button != null)
+            {
+                button.onClick.RemoveListener(HandleOnButtonClicked);
+            }
+        }
+
+        #endregion
+
+        #region Copy Method
+
+        private void CopyToClipboard()
+        {
+            if (copyTextField != null)
+            {
+                GUIUtility.systemCopyBuffer = copyTextField.text;
+            }
+            else if (button.GetComponent<TextMeshProUGUI>() != null)
+            {
+                GUIUtility.systemCopyBuffer = button.GetComponent<TextMeshProUGUI>().text;
+            }
+        }
+
+        #endregion
+
+        #region Handlers
+
+        private void HandleOnButtonClicked()
+        {
+            CopyToClipboard();
+        } 
+
+        #endregion
+    } 
+}
